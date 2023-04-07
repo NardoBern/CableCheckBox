63411e75abc749a719b8dd9b853b5ad4e3b59414
Giuseppe Parodi <giuseppe.parodi@arol.com>
2023-04-07 14:16:43 +0200

    Added input for Sercos check of all the wire couples

--- testCableFixedWithSquare/testCableFixedWithSquare.ino
+++ testCableFixedWithSquare/testCableFixedWithSquare.ino
@@ -16,7 +16,10 @@ bool xClock;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(2000000);
-  pinMode(7, INPUT_PULLUP); // checked input
+  pinMode(7, INPUT_PULLUP); // checked Sercos wire couple #1
+  pinMode(6, INPUT_PULLUP); // checked Sercos wire couple #2
+  pinMode(5, INPUT_PULLUP); // checked Sercos wire couple #3
+  pinMode(4, INPUT_PULLUP); // checked Sercos wire couple #4
   pinMode(1, INPUT_PULLUP); // reset
 
   pinMode(13, OUTPUT); // error output
@@ -35,21 +38,46 @@ void clockGen(){
 }
 
 void readInput(){
-  inVal = digitalRead(7);
+  inVal1 = digitalRead(7);
+  inVal2 = digitalRead(6);
+  inVal3 = digitalRead(5);
+  inVal4 = digitalRead(4);
   Reset = digitalRead(1);
 }
 
 void checkCableInter(){
-  if ((inVal) && !(inOldVal) && !(xTestFail)){
+  /* Check wire couple #1 */
+  if ((inVal1) && !(inOldVal1) && !(xTestFail)){
     xTestFail = true;
     xResetReq = true;
     Serial.println("Check interruption failed");
   }
-  inOldVal = inVal;
+  inOldVal1 = inVal1;
+  /* Check wire couple #2 */
+  if ((inVal2) && !(inOldVal2) && !(xTestFail)){
+    xTestFail = true;
+    xResetReq = true;
+    Serial.println("Check interruption failed");
+  }
+  inOldVal2 = inVal2;
+  /* Check wire couple #3 */
+  if ((inVal3) && !(inOldVal3) && !(xTestFail)){
+    xTestFail = true;
+    xResetReq = true;
+    Serial.println("Check interruption failed");
+  }
+  inOldVal3 = inVal3;
+  /* Check wire couple #4 */
+  if ((inVal4) && !(inOldVal4) && !(xTestFail)){
+    xTestFail = true;
+    xResetReq = true;
+    Serial.println("Check interruption failed");
+  }
+  inOldVal4 = inVal4;
 }
 
 void checkCableCont(){
-  if ((inVal) && !(xTestContFail) && !(xTestFail)){
+  if (((inVal1) || (inVal2) || (inVal3) || (inVal4))&& !(xTestContFail) && !(xTestFail)){
     xTestContFail = true;
     xResetReq = true;
     Serial.println("Check continuity failed");
